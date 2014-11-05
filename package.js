907e024c28e25acedb8e250f39d34c800232c932
Andrew Straw <strawman@astraw.com>
2014-11-05 22:36:11 +0100

    update template to serve our files

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 Package.describe({
   name: 'astraw:jquery-fleximages',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  summary: "Use Pixabay's jQuery-flexImages package in Meteor",
+  version: '0.0.1'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('astraw:jquery-fleximages.js');
-});
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('astraw:jquery-fleximages');
-  api.addFiles('astraw:jquery-fleximages-tests.js');
+  api.use('jquery');
+
+  var path = Npm.require('path');
+  var asset_path = path.join('jQuery-flexImages');
+  api.addFiles(path.join(asset_path, 'jquery.flex-images.css'), 'client');
+  api.addFiles(path.join(asset_path, 'jquery.flex-images.min.js'), 'client');
 });
